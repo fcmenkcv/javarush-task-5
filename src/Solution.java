001955de35e76fd28a7b916ba0856745dc1cf5cc
fcmenkcv <fcmenkcv@gmail.com>
2024-08-03 23:14:38 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task01.task0103;
+
+/*
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // напишите тут ваш код
+        int a = 3126;
+        int b = 8;
+
+        System.out.println(a - b);
+    }
+}
